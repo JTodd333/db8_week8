0f411b2873c1fb93b8a8607fe8ee179e825a2ee9
Jesse Todd <jessetodd@rocketmortgage.com>
2022-09-20 16:56:41 -0400

    1st commit, week 8 lab 1 and mostly 2

---
+++ CoffeeShopRegLab/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CoffeeShopRegLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessRegister(string firstName)
+        {
+            ViewBag.FirstName = firstName;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
